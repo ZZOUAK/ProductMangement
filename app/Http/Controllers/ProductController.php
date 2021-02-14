c8ca09f0f5be74e4bd3cd225fe1c21afe691f36e
ZZOUAK <freeboysteam51@gmail.com>
2021-02-14 23:21:18 +0100

    methode store for product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
@@ -14,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return view('home');
     }
 
     /**
@@ -35,7 +37,48 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $product = new Product();
+        request()->validate([
+            'name' => 'required|string|unique:products,name',
+            'description' => 'required|string',
+            'price' =>'required|numeric',
+            'image' =>'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'Category' =>'required|string|exists:categories,name',
+
+        ]);
+
+
+        $category=Category::where('name',strip_tags(request('category')))->get();
+        $categoryId=$category[0]->id;
+
+        $product->category_id=$categoryId;
+        $product->name=strip_tags(request('name'));
+        $product->description=strip_tags(request('description'));
+        $product->price=strip_tags(request('price'));
+        if ($request->has('image')) {
+            // Get image file
+            $image = $request->file('image');
+            // Make a image name based on user name and current timestamp
+
+            $name = Str::slug($request->input('name').'_'.time());
+            // Define folder path
+            $folder = 'public/storage/images';
+            // Make a file path where image will be stored [ folder path + file name + file extension]
+            $filePath = $folder . '/'.$name. '.' . $image->getClientOriginalExtension();
+            // Upload image
+            $this->uploadOne($image, $folder, 'public', $name);
+            // Set user profile image path in database to filePath
+            $product->image = $filePath;
+        }
+
+
+
+        $product->save();
+
+        $request->session()->flash('status', 'Enregistré avec succès !  ');
+
+            return redirect(route('home'));
+
     }
 
     /**
